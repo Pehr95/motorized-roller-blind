ed356fc95792b68df6a74d5a0f29c1d4dd5419a9
Pehr95 <pehr.norten@gmail.com>
2024-11-04 21:54:44 +0100

    heeej

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,20 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
 
-
+// Button pins
 #define UP_BUTTON_PIN 2
 #define DOWN_BUTTON_PIN 5
+
+// Motor pins
 #define MOTOR_IN1_PIN 4
 #define MOTOR_IN2_PIN 3
 #define MOTOR_PWM_CHANNEL1 0
 #define MOTOR_PWM_CHANNEL2 1
 
-#define INA219_SDA_PIN 6 // Change this to your desired SDA pin
-#define INA219_SCL_PIN 7 // Change this to your desired SCL pin
+// INA219 pins
+#define INA219_SDA_PIN 6
+#define INA219_SCL_PIN 7
 
-bool setupDone = false;
 bool motorStalled = false;
 Adafruit_INA219 ina219;
 
@@ -58,6 +60,7 @@ void setup() {
   while (!Serial) {
     delay(10);
   }
+  // Initialize pins
   pinMode(UP_BUTTON_PIN, INPUT);
   pinMode(DOWN_BUTTON_PIN, INPUT);
   pinMode(MOTOR_IN1_PIN, OUTPUT);
@@ -67,46 +70,37 @@ void setup() {
   ledcAttachPin(MOTOR_IN1_PIN, MOTOR_PWM_CHANNEL1);
   ledcAttachPin(MOTOR_IN2_PIN, MOTOR_PWM_CHANNEL2);
   Wire.begin(INA219_SDA_PIN, INA219_SCL_PIN);
+
   delay(1000);
-  Serial.println("Setup done");
+  if (!ina219.begin()) {
+    Serial.println("Failed to find INA219 chip");
+    while (true) {
+      delay(10);
+    }
+  }
+  Serial.println("Found INA219. Setup done!");
 }
 
 void loop() {
-
-
-  if(!setupDone) {
-    delay(2000);
-    Serial.println("Setting up INA219");
-    if (!ina219.begin()) {
-      Serial.println("Failed to find INA219 chip");
-      while (1) {
-        delay(10);
-      }
-    }
-    setupDone = true;
-  }
-  /*
   if (ina219.getCurrent_mA() > 200) {
     motorStalled = true;
     Serial.print("Bus Voltage:   "); Serial.print(ina219.getBusVoltage_V()); Serial.println(" V");
     Serial.print("Current mA:    "); Serial.print(ina219.getCurrent_mA()); Serial.println(" mA");
   }
-  */
 
   if(!motorStalled) {
     if (digitalRead(UP_BUTTON_PIN) == HIGH) {
     setMotor(255, UP);
     Serial.println("UP");
-  }
-  if (digitalRead(DOWN_BUTTON_PIN) == HIGH) {
-    setMotor(255, DOWN);
-    Serial.println("DOWN");
-  }
-  delay(10);
+    }
+    if (digitalRead(DOWN_BUTTON_PIN) == HIGH) {
+      setMotor(255, DOWN);
+      Serial.println("DOWN");
+    }
 
-  if (digitalRead(UP_BUTTON_PIN) == LOW && digitalRead(DOWN_BUTTON_PIN) == LOW) {
-    setMotor(0, COAST);
-  }
+    if (digitalRead(UP_BUTTON_PIN) == LOW && digitalRead(DOWN_BUTTON_PIN) == LOW) {
+      setMotor(0, COAST);
+    }
   }
   else {
     setMotor(0, COAST);
